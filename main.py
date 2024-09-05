528c1ade2a82fb6732e8dafdfc8f81f0b7bb3733
Koopavocelot <72164093+Koopavocelot@users.noreply.github.com>
2024-09-05 11:57:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from unityparser import UnityDocument
-import os
 
 
 def run():
@@ -7,23 +6,16 @@ def run():
     unity_document = UnityDocument.load_yaml(project_settings_file_path)
     projectsettings_monobehaviour_document = unity_document.entry
 
-    bundle_version = ""
     bundle_code = 0
     try:
-        bundle_version = projectsettings_monobehaviour_document.bundleVersion
         bundle_code = projectsettings_monobehaviour_document.AndroidBundleVersionCode
     except AttributeError:
         print("Failed to find bundleVersion in ProjectSettings.asset")
         return
 
-    print("bundle_version: " + str(bundle_version))
     print("bundle_code: " + str(bundle_code))
 
-    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-        print(f'bundle-version={bundle_version}', file=fh)
-
-    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-        print(f'bundle-code={bundle_code}', file=fh)
+    print(f"::set-output name=bundle-code::{bundle_code}")
 
     return
 
